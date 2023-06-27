d5ca7d09a85942ca1c60f157a211099803f38ac4
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-27 08:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 		var version = parseFloat(navigator.appVersion);
 
 		// Create the message
-		var message = "You are using " + browserName ;
+		var message = "You are using " + browserName +" version "+version; ;
 
 		// Display the message on the page
 		var browserInfoElement = document.getElementById("browser-info");
