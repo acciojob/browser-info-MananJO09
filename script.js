26dc522f8969383fec707501e83ee381c6dbc42c
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-30 19:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,3 @@
 	var browserInfoElement = document.getElementById("browser-info");
 	browserInfoElement.textContent = message;
  }
-//You are using Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
-//(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 version 5
