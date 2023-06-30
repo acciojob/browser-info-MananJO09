c984002c72bd49ef495b5020b407c9381f4ddf18
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-30 12:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
  function displayBrowserInfo() {
 		// Get the browser name and version
-		var browserName = navigator.userAgent;
-		var version = parseFloat(navigator.appVersion);
+	var browserName = navigator.userAgent;
+	var version = parseFloat(navigator.appVersion);
 
-		// Create the message
-		var message = "You are using " + browserName +" version "+version; ;
+	// Create the message
+	var message = "You are using " + browserName +" version "+version; ;
 
-		// Display the message on the page
-		var browserInfoElement = document.getElementById("browser-info");
-		browserInfoElement.textContent = message;
+	// Display the message on the page
+	var browserInfoElement = document.getElementById("browser-info");
+	browserInfoElement.textContent = message;
  }
