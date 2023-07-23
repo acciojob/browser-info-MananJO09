1a969a5e3bc84dd0a31322cabceeb85532548fda
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-07-23 14:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-// Get the browser name and version
-var browserName = navigator.appName;
-var version = navigator.appVersion;
+// Function to get the browser name and version
+function getBrowserInfo() {
+const userAgent = navigator.userAgent;
+let browserName;
+let version;
 
-// Create the message
-var message = " \n\t\t\n\tYou are using " + browserName +"\n\t\t\n\t version "+version; ;
+if (userAgent.indexOf("Firefox") > -1) {
+browserName = "Mozilla Firefox";
+} else if (userAgent.indexOf("Chrome") > -1) {
+browserName = "Google Chrome";
+} else if (userAgent.indexOf("Safari") > -1) {
+browserName = "Apple Safari";
+} else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident/") > -1) {
+browserName = "Internet Explorer";
+} else {
+browserName = "Unknown Browser";
+}
 
-// Display the message on the page
-var browserInfoElement = document.getElementById("browser-info");
-browserInfoElement.innerHTML = message;
- 
+const start = userAgent.indexOf(browserName) + browserName.length + 1;
+const end = userAgent.indexOf(" ", start);
+version = userAgent.substring(start, end);
+
+return {
+name: browserName,
+version: version,
+};
+}
+
+// Display the browser information on the page
+function displayBrowserInfo() {
+const browserInfoElement = document.getElementById("browser-info");
+const browserInfo = getBrowserInfo();
+const message = `You are using ${browserInfo.name} version ${browserInfo.version}.`;
+browserInfoElement.textContent = message;
+}
+
+displayBrowserInfo();
