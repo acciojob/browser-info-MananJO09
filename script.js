8698811e0e1e464025cf9c05a5b5c88c244b8aca
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-07-23 13:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var browserName = navigator.appName;
 var version = navigator.appVersion;
 
 // Create the message
-var message = "You are using " + browserName;
+var message = " \n\t\t\n\tYou are using " + browserName +"\n\t\t\n\t version "+version; ;
 
 // Display the message on the page
 var browserInfoElement = document.getElementById("browser-info");
